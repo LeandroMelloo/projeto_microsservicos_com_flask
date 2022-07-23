2fd30876fec86a819552199b84b36f885a6c335c
LeandroMelloo <leandromoreno1909@hotmail.com>
2022-07-23 18:43:20 -0300

    Crianção do APP de avaliações e comentarios

--- core/models.py
+++ core/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from recursos.models import Recurso
+from comentarios.models import Comentario
 
 
 class PontoTuristico(models.Model):
     nome = models.CharField(max_length=150)
     descricao = models.TextField()
     aprovado = models.BooleanField(default=False)
+    recursos = models.ManyToManyField(Recurso)
+    comentario = models.ManyToManyField(Comentario)
     
     def __str__(self):
         return self.nome
