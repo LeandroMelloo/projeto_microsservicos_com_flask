75769c219f493efd2d6207d6c14d4d290d658ce9
LeandroMelloo <leandromoreno1909@hotmail.com>
2022-07-23 21:16:46 -0300

    Criando a consulta de pontos turisticos

--- core/models.py
+++ core/models.py
@@ -12,7 +12,7 @@ class PontoTuristico(models.Model):
     recursos = models.ManyToManyField(Recurso)
     comentarios = models.ManyToManyField(Comentario)
     avaliacoes = models.ManyToManyField(Avaliacao)
-    endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE)
+    endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE, null=True, blank=True)
     
     def __str__(self):
         return self.nome
